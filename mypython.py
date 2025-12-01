30915ab8426d3d619073689cd826e326f90c9629
Shahbaz-Arcanum <arcaneinsights39@gmail.com>
2025-12-01 23:12:28 +0600

    mypython.py

--- mypython.py
+++ mypython.py
@@ -1,20 +1,23 @@
 
-
 # 🏦 Mini Budget Manager Challenge #
 
-income = 50000
-rent = 12000
-food = 8000
-transport = 5000
-entertainment = 3000
+income = int(input("Enter your monthly income: "))
+rent = int(input("Enter your rent: "))
+food = int(input("Enter your food expenses: "))
+transport = int(input("Enter your transport expenses: "))
+entertainment = int(input("Enter your entertainment expenses: "))
+
+# Calculate total expenses
 
 total_expenses = rent + food + transport + entertainment
 print(f"Your Total Expense is: ${total_expenses}")
 
+# Calculate remaining moneyv
 
 remaining_money = income - total_expenses
 print(f"Your remaining money is: ${remaining_money}")
 
+# Store expenses in a dictionary
 
 expenses ={
             
@@ -24,10 +27,13 @@ expenses ={
             "Entertainment" : entertainment
 }
 
+# Calculate percentage of income spent on each category
+
 for item, amount in expenses.items():
     percent = round((amount * 100) / income, 2)
     print(f"You spent {percent}% of your money on {item}")
 
+# Check savings condition
 
 if remaining_money >= 10000:
     print('You are saving well this month!')
@@ -37,8 +43,8 @@ elif 0 < remaining_money < 10000:
 elif remaining_money < 0:
     print("You are overspending! Reduce expenses!")
 
+# Summary
 
 all_together = (f"A family spends their money on different things. After the month ends, their income is ${income}, total expenses are ${total_expenses}, so they have ${remaining_money} left.")
-
 
 print(all_together)
